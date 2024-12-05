10f4fc3ab210cfc3a7fa9dc878c9be6564618ed9
Richard <39804778+RSJAL@users.noreply.github.com>
2024-12-05 10:37:16 -0500

    Update fe_segmentation.R

--- FE_Segmentation/fe_segmentation.R
+++ FE_Segmentation/fe_segmentation.R
@@ -29,26 +29,20 @@ init <- data.frame(Name = character(),
 
 for (i in 1:length(tables)) {
   
-        tables[[i]]$House <- i
-        init <- rbind(init, tables[[i]])
-        
-  }
-
+  tables[[i]]$House <- i
+  init <- rbind(init, tables[[i]])
+  
+}
 
 fe_stats <- data.frame(init)
 fe_stats$House <- as.factor(fe_stats$House)
 
-fe_stats <- fe_stats[!grepl("(NPC)", fe_stats$Name),]
-#fe_stats <- fe_stats[!grepl("Cyril", fe_stats$Name),]
-
+fe_stats <- fe_stats[!grepl("(NPC)|Cyril", fe_stats$Name),]
 
 rownames(fe_stats) <- fe_stats[,1]
 fe_stats <- fe_stats[-1]
 
-#fe_stats[30,1:9] <- fe_stats[32,1:9] + 20
-
 fe_scale <- data.frame(scale(fe_stats[-10]))
-
 
 # Segmentation..................................................................................................................
 
@@ -58,17 +52,11 @@ fviz_nbclust(fe_scale, kmeans, method = "silhouette")
 fviz_nbclust(fe_scale, kmeans, method = "gap_stat")
 fviz_nbclust(fe_scale, kmeans, method = "wss")
 
-km2 <- kmeans(fe_scale, centers = 2, nstart = 25)
-km3 <- kmeans(fe_scale, centers = 3, nstart = 25)
+
 km4 <- kmeans(fe_scale, centers = 4, nstart = 25)
-km5 <- kmeans(fe_scale, centers = 5, nstart = 25)
 
-
-
-fviz_cluster(km2, data = fe_scale, ellipse.type = "norm", ggtheme = theme_minimal(), repel = TRUE)
-fviz_cluster(km3, data = fe_scale, ellipse.type = "norm", ggtheme = theme_minimal(), repel = TRUE)
 fviz_cluster(km4, data = fe_scale, ellipse.type = "norm", ggtheme = theme_minimal(), repel = TRUE)
-fviz_cluster(km5, data = fe_scale, ellipse.type = "norm", ggtheme = theme_minimal(), repel = TRUE)
+
 
 # Data Analysis...................................................................................................................
 
@@ -79,20 +67,12 @@ aggregate(fe_stats[1:9], by = list(Cluster = fe_stats$cluster), mean)
 
 aggregate(fe_stats[1:9], by = list(House = fe_stats$House), mean)
 
-# Significant predictive model attained by step-wise regression
-regr <- lm(as.numeric(cluster) ~ HP + Mag + Dex + Def, data = fe_stats)
-
 # Data Viz........................................................................................................................
 
 corrplot(cor(fe_stats[,1:9]))
 
 distance <- get_dist(fe_stats[1:9])
 fviz_dist(distance, gradient = list(low = "#00AFBB", mid = "white", high = "#FC4E07"))
-
-barplot(table(fe_stats$House), main = "Members of Each House", ylab = "Count", xlab = "House", col = c("green", "red", "blue", "yellow", "snow", "navy", "grey"))
-barplot(table(fe_stats$cluster), main = "Members of Each House", ylab = "Count", xlab = "House")
-
-xyplot(Str ~ Mag, group=cluster, data=fe_stats, auto.key=list(space="right"), jitter.x=TRUE, jitter.y=TRUE, pch = 19)
 
 plot_ly(
   text = rownames(fe_stats),
